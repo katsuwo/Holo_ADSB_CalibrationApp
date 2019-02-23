4c9caa86ba601147ff87aa9cc32e2add19a7126a
katsuwo <katsuwo7720@gmail.com>
2019-02-24 04:40:26 +0900

    Initial Commit

---
+++ Holo_ADSB_CalibrationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Holo_ADSB_CalibrationApp
+//
+//  Created by katsuwo on 2019/02/24.
+//  Copyright © 2019 katsuwo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
